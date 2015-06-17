0f7bce4f9dc1c3f8b0e807355d477da286917f7c
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-06-17 12:59:24 +0100

    fixed karma configuration, added angular as npm dependency

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,52 @@
 'use strict';
 
-module.exports = function(config) {
+module.exports = function (config) {
 
-  config.set({
-    autoWatch : false,
+    config.set({
+        autoWatch: false,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['PhantomJS'],
+        browsers: ['PhantomJS'],
 
-    files: [
-    'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js',
-    'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-mocks.js',
-
-          'src/**/*Module.js',
-          'src/**/*.js',
-          'src/**/*.spec.js',
+        files: [
+            'node_modules/angular/angular.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'src/**/*Module.js',
+            'src/**/*.js',
+            'src/**/*.spec.js'
         ],
 
-    
 
-    plugins : [
-        'karma-phantomjs-launcher',
-        'karma-jasmine',
-        'karma-html-reporter',
-        'karma-junit-reporter',
-         'karma-ng-html2js-preprocessor'
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-html-reporter',
+            'karma-junit-reporter',
+            'karma-ng-html2js-preprocessor'
 
-    ],
+        ],
 
-    reporters: ['html','progress', 'junit'],
-    preprocessors: {
-              '**/*.html': ['ng-html2js']
-          },
+        reporters: ['html', 'progress', 'junit'],
+        preprocessors: {
+            '**/*.html': ['ng-html2js']
+        },
 
-    ngHtml2JsPreprocessor: {
-              // setting this option will create only a single module that contains templates
-              // from all the files, so you can load them all with module('foo')
-             moduleName: 'templates',
-             stripPrefix: ".*/templates/",
-    prependPrefix: "templates/",
-         },
+        ngHtml2JsPreprocessor: {
+            // setting this option will create only a single module that contains templates
+            // from all the files, so you can load them all with module('foo')
+            moduleName: 'templates',
+            stripPrefix: ".*/templates/",
+            prependPrefix: "templates/"
+        },
 
 
-    // the default configuration
-    junitReporter: {
-      outputFile: 'target/test/test-results.xml',
-      suite: ''
-    },
+        // the default configuration
+        junitReporter: {
+            outputFile: 'target/test/test-results.xml',
+            suite: ''
+        },
 
-    singleRun : true
-
-    //logLevel: 'debug'
-  });
+        singleRun: true
+    });
 };
